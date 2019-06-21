43750cb9f4124c28edb96068cc8bb71e63d101f2
Djordje Ivkovic <ivkovicdjordje1997@gmail.com>
2019-06-21 22:05:40 +0200

    flights

--- WebProjekat/WebContent/app/flights.js
+++ WebProjekat/WebContent/app/flights.js
@@ -1,7 +1,22 @@
 Vue.component("flights", {
   data: function() {
     return {
-    	flights: []
+    	flights: [],
+    	destinations:[],
+    	flightToAdd:{
+    		number: "",
+    		price: 0,
+    		airplane: "",
+    		firstSize: 0,
+    		businessSize: 0,
+    		economySize: 0,
+    		date: "",
+    		type: "",
+    		start: "",
+    		end: ""
+    		
+    		
+    	}
     }
   },
   template:`
@@ -36,10 +51,9 @@ Vue.component("flights", {
             </tr>
         </table>
         
-        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#addFlightModal">Add flight</button>
+        <button id="addModal" v-on:click="addM()" type="button" class="btn btn-info">Add flight</button>
         
         <!--modal dodavanja-->
-        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#addFlightModal">Add flight</button>
         <div class="modal fade" id="addFlightModal" role="dialog">
 	  		<div class="modal-dialog modal-sm">
       			<div class="modal-content">
@@ -48,36 +62,41 @@ Vue.component("flights", {
           				<button type="button" class="close" data-dismiss="modal">&times;</button>
         			</div>
         			<div class="modal-body">
+        				<form id="addFlightForm">
+        				  <label for="number"><b>Flight number :</b></label>
+						  <input v-model:value="flightToAdd.number" type="number" class="form-control" id="number">
+        				  
         				  <label for="startDestinationAdd"><b>Start destination :</b></label>
-        				  <select class="form-control" id="startDestinationAdd"><option>1</option></select>
+        				  <select v-model:value="flightToAdd.start" class="form-control" id="startDestinationAdd"><option v-for="d in destinations">{{ d.name }}</option></select>
         				  
         				  <label for="endDestinationAdd"><b>End destination :</b></label>
-        				  <select class="form-control" id="endDestinationAdd"><option>1</option></select>
+        				  <select v-model:value="flightToAdd.end" class="form-control" id="endDestinationAdd"><option v-for="d in destinations">{{ d.name }}</option></select>
         				  
           				  <label for="dateAdd"><b>Date :</b></label>
-						  <input type="date" class="form-control" id="dateAdd">
+						  <input v-model:value="flightToAdd.date" type="date" class="form-control" id="dateAdd">
 								
 						  <label for="priceAdd"><b>Price :</b></label>
-						  <input type="number" class="form-control" id="priceAdd">
+						  <input v-model:value="flightToAdd.price" type="number" class="form-control" id="priceAdd">
 						  
 						  <label for="typeAdd"><b>Flight type :</b></label>
-        				  <select class="form-control" id="typeAdd"></select>
+        				  <select v-model:value="flightToAdd.type" class="form-control" id="typeAdd"><option>CHARTER</option><option>REGIONAL</option><option>OVERSEA</option></select>
 								
 						  <label for="airplaneAdd"><b>Airplane :</b></label>
-						  <input type="text" class="form-control" id="airplaneAdd">
+						  <input v-model:value="flightToAdd.airplane" type="text" class="form-control" id="airplaneAdd">
 						  
 						  <label for="businessAdd"><b>Business class seats :</b></label>
-						  <input type="number" class="form-control" id="businessAdd">
+						  <input v-model:value="flightToAdd.businessSize" type="number" class="form-control" id="businessAdd">
 						  
 						  <label for="firstAdd"><b>First class seats :</b></label>
-						  <input type="number" class="form-control" id="firstAdd">
+						  <input v-model:value="flightToAdd.firstSize" type="number" class="form-control" id="firstAdd">
 						  
 						  <label for="economyAdd"><b>Economy class seats :</b></label>
-						  <input type="number" class="form-control" id="economyAdd">
+						  <input v-model:value="flightToAdd.economySize" type="number" class="form-control" id="economyAdd">
+						</form>
         			</div>
         			<div class="modal-footer">
-          				<button type="button" class="btn btn-success" onclick="">Add</button>
-          				<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+          				<button type="button" class="btn btn-success" v-on:click="createFlight">Add</button>
+          				<button type="button" id="closeAddFlightModal" class="btn btn-danger" data-dismiss="modal">Close</button>
         			</div>
       			</div>
 	  		</div>
@@ -93,6 +112,9 @@ Vue.component("flights", {
           				<button type="button" class="close" data-dismiss="modal">&times;</button>
         			</div>
         			<div class="modal-body">
+        				  <label for="number"><b>Flight number :</b></label>
+						  <input type="number" class="form-control" id="number">
+						  
         				  <label for="startDestinationEdit"><b>Start destination :</b></label>
         				  <select class="form-control" id="startDestinationEdit"></select>
         				  
@@ -121,7 +143,7 @@ Vue.component("flights", {
 						  <input type="number" class="form-control" id="economyEdit">
         			</div>
         			<div class="modal-footer">
-          				<button type="button" class="btn btn-success" onclick="">Edit</button>
+          				<button type="button" class="btn btn-success" >Edit</button>
           				<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
         			</div>
       			</div>
@@ -132,9 +154,45 @@ Vue.component("flights", {
     </div>
    </div>
   `,
-  methods: {
+  methods: {getFlights: function() {
+	  axios.get("/WebProjekat/api/flights", {headers: {"responseType":"json"}})
+      .then(response => {
+    	  this.flights = [];
+          this.flights = response.data;
+      })
+      .catch(response => {
+          toastr.error("Something went wrong.");
+      });
+  },
 	  createFlight: function() {
-		  // json copy
+		  //var form = document.getElementById("addFlightForm");
+          //var formData = new FormData(form);
+          //if (this.validateForm(formData)) {
+		  	  console.log(this.flightToAdd);
+              axios.post("/WebProjekat/api/flights", this.flightToAdd, {headers:{"Content-Type":"json"}})
+                   .then(response => {
+                       toastr.success("Flight added.")
+                       this.destinations.push(response.data);
+                       document.getElementById("closeAddFlightModal").click();
+                   })
+                   .catch(response => {
+                	   
+                       toastr.error("Something is wrong with your request.");
+                       console.log(response);
+                   })
+          //} else {
+             // toastr.warning('Please enter all fields!');
+          //}
+	  },
+	  getDestinations: function() {
+		  axios.get("/WebProjekat/api/destinations", {headers: {"responseType":"json"}})
+          .then(response => {
+        	  this.destinations = [];
+              this.destinations = response.data;
+          })
+          .catch(response => {
+              toastr.error("Something went wrong.");
+          });
 	  },
 	  
 	  editFlight: function(flight) {
@@ -144,12 +202,17 @@ Vue.component("flights", {
 	  
 	  deleteFlight: function(flight) {
 		  // json copy
-	  }
-  },
+	  },
+  
   	  showEditModal: function() {
   		$("#editFlightModal").modal();  
   	  },
+  	addM: function() {
+  		$("#addFlightModal").modal();
+  		this.getDestinations();
+  	  }},
   mounted: function() {
-
+	  this.getFlights();
+	  
   }
 });
