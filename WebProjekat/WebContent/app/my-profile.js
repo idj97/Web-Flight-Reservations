794e98b58e22d70dd621fafd96173c248be61991
Djordje Ivkovic <ivkovicdjordje1997@gmail.com>
2019-06-21 17:19:02 +0200

    myprofile forma

--- WebProjekat/WebContent/app/my-profile.js
+++ WebProjekat/WebContent/app/my-profile.js
@@ -1,16 +1,50 @@
 Vue.component("my-profile", {
   data: function() {
-    return {}
+    return {
+    	user: {
+        	name: "",
+        	surname: "",
+        	phone: "",
+        	email: ""
+        }
+    }
   },
   template:`
-    <div>
-      <h1> My profile component </h1>
+    <div class="myProfileDiv">
+      <label for="firstname"><b>Firstname :</b></label>
+	  <input type="text" class="form-control" id="name">
+			
+	  <label for="surname"><b>Surname :</b></label>
+	  <input type="text" class="form-control" id="surname">
+	  
+	  <label for="phone"><b>Phone :</b></label>
+	  <input type="text" class="form-control" id="phone">
+			
+	  <label for="email"><b>Email :</b></label>
+	  <input type="text" class="form-control" id="email">
+	  <br>
+	  <button class="btn btn-primary"  onclick="">Save</button>
     </div>
   `,
   methods: {
+	  
+	  getLoggedUser: function() {
+		  axios.get("/WebProjekat/api/auth/detail", {headers: {"responseType":"json"}})
+          .then(response => {
+              this.user = response.data;
+          })
+          .catch(response => {
+              toastr.error("Something went wrong.");
+          });
+	  }
 
   },
   mounted: function() {
+	  lUser = JSON.parse(localStorage.getItem("user"));
+	  $("#name").val(lUser.name);
+	  $("#surname").val(lUser.surname);
+	  $("#phone").val(lUser.phone);
+	  $("#email").val(lUser.email);
 
   }
 });
